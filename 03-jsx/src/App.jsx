1a388838dcb07fbce539202cea4ec1077b7b32a4
Naveen Raj <naveen@Naveens-MacBook-Air.local>
2023-08-03 17:44:33 -0400

    working with JSX

---
+++ 03-jsx/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Pet from "./components/Pet";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>Adopt Me</h1>
+        <Pet name="Scoob" animal="dog" breed="lab" />
+        <Pet name="Jackie" animal="dog" breed="German" />
+        <Pet name="Jimmy" animal="dog" breed="Yorkshire" />
+      </div>
+    </>
+  );
+};
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+root.render(React.createElement(App));
