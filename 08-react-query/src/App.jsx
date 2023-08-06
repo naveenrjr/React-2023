3af10a0e54931a7498e9457950026cd87aee0f11
Naveen Raj <naveen@Naveens-MacBook-Air.local>
2023-08-06 15:15:59 -0400

    added react-query

---
+++ 08-react-query/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SearchParams from "./components/SearchParams";
+import Details from "./components/Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <header>
+          <Link to="/">Adopt me! </Link>
+        </header>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
+  );
+};
+
+const container = document.getElementById("root");
+
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
